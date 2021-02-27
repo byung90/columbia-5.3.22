6b094a9f1099ffd634eb2ce02563634b602946ea
Byungwook Yoon <byung90@gmail.com>
2021-02-27 10:46:48 -0500

    complete

---
+++ Solved/assets/js/script.js
@@ -0,0 +1,48 @@
+var formEl = $('#skills-form');
+var nameInputEl = $('#skill-name');
+var dateInputEl = $('#datepicker');
+var skillsListEl = $('#skills-list');
+
+var printSkills = function (name, date) {
+  var listEl = $('<li>');
+  var listDetail = name.concat(' on ', date);
+  listEl.addClass('list-group-item').text(listDetail);
+  listEl.appendTo(skillsListEl);
+};
+
+var handleFormSubmit = function (event) {
+  event.preventDefault();
+
+  var nameInput = nameInputEl.val();
+  var dateInput = dateInputEl.val();
+
+  if (!nameInput || !dateInput) {
+    console.log('You need to fill out the form!');
+    return;
+  }
+
+  printSkills(nameInput, dateInput);
+
+  nameInputEl.val('');
+  dateInputEl.val('');
+};
+
+formEl.on('submit', handleFormSubmit);
+
+// Add Autocomplete widget here
+$(function () {
+  var availableSkills = [
+    'HTML',
+    'CSS',
+    'Javascript',
+    'Jquery'
+  ];
+  nameInputEl.autocomplete({
+    source: availableSkills
+  });
+});
+
+// Add Datepicker widget here
+$(function () {
+  dateInputEl.datepicker();
+});
